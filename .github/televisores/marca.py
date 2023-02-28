7ddb1ce44e228bcbad2573d2499d6281bdb8003d
Milord01 <124930730+Milord01@users.noreply.github.com>
2023-02-27 20:40:05 -0500

    tv

---
+++ .github/televisores/marca.py
@@ -0,0 +1,8 @@
+class Marca:
+    def __init__ (self, nombre):
+        self.__nombre=nombre
+    def setNombre(self, nombre):
+        self.__nombre=nombre
+
+    def getNombre(self):
+        return self.__nombre
